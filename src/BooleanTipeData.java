17fa2b41e84fe3d9c148729a33817ed2ade34e85
alif <alifrazanit@gmail.com>
2022-07-12 21:47:16 +0700

    character & boolean

---
+++ src/BooleanTipeData.java
@@ -0,0 +1,8 @@
+public class BooleanTipeData {
+    public static void main(String[] args) {
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println(benar);
+    }
+}
---
+++ src/Character.java
@@ -0,0 +1,9 @@
+public class Character {
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = 'b';
+        char c = 'c';
+
+        System.out.println(a);
+    }
+}
